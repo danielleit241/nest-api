4cd0e0022dbaafd5fca85af0c91461e1e0c4ec61
danielleit241 <danielleee241@gmail.com>
2025-06-26 21:48:50 +0700

    feat: enhance user controller with JWT guard and add edit user functionality

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,17 +1,28 @@
 import {
   Controller,
   Get,
+  Put,
   UseGuards,
 } from '@nestjs/common';
 import { User } from 'generated/prisma';
 import { GetUser } from 'src/auth/decorator/get-user.decorator';
 import { JwtGuard } from 'src/auth/guard';
 
+@UseGuards(JwtGuard)
 @Controller('users')
 export class UserController {
-  @UseGuards(JwtGuard)
   @Get('me')
   getMe(@GetUser() user: User) {
+    console.log(user);
     return user;
+  }
+
+  @Put()
+  editUser(@GetUser('id') userId: number) {
+    console.log(userId);
+    return {
+      message: 'User updated successfully',
+      userId: userId,
+    };
   }
 }
