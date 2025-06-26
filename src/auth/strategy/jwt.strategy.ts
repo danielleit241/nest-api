3ecf41e97db6df576183947ef2918f78ff8f92d4
danielleit241 <danielleee241@gmail.com>
2025-06-26 17:11:01 +0700

    feat: implement JWT authentication with user guard and decorator, add user controller and module

---
+++ src/auth/strategy/jwt.strategy.ts
@@ -0,0 +1,47 @@
+import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { PassportStrategy } from '@nestjs/passport';
+import { Prisma } from 'generated/prisma';
+import {
+  ExtractJwt,
+  Strategy,
+} from 'passport-jwt';
+import { PrismaService } from 'src/prisma/prisma.service';
+
+@Injectable()
+export class JwtStategy extends PassportStrategy(
+  Strategy,
+) {
+  constructor(
+    private config: ConfigService,
+    private prisma: PrismaService,
+  ) {
+    const jwtSecret =
+      config.get<string>('JWT_SECRET');
+    if (!jwtSecret) {
+      throw new Error(
+        'JWT_SECRET is not defined in configuration',
+      );
+    }
+    super({
+      jwtFromRequest:
+        ExtractJwt.fromAuthHeaderAsBearerToken(),
+      secretOrKey: jwtSecret,
+    });
+  }
+  async validate(payload: {
+    sub: number;
+    email: string;
+  }) {
+    const user =
+      await this.prisma.user.findUnique({
+        where: { id: payload.sub },
+      });
+    if (!user) {
+      return null;
+    }
+    const { hash: _hash, ...userWithoutHash } =
+      user;
+    return userWithoutHash;
+  }
+}
